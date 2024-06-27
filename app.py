8048fc970a9c5b34d62786c24093bf8216642d2a
GetomG <zai.thanakrit2@gmail.com>
2024-06-27 22:43:46 +0700

    write objects?

--- app.py
+++ app.py
@@ -86,6 +86,10 @@ if source_radio == settings.IMAGE:
                 res_plotted = res[0].plot()[:, :, ::-1]
                 st.image(res_plotted, caption='Detected Image',
                          use_column_width=True)
+                
+                objects = res[0].objects
+                st.write("Detected Objects: ", objects)
+
                 try:
                     with st.expander("Detection Results"):
                         for box in boxes:
